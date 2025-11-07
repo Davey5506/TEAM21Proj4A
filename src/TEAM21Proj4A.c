8701b5bd40304ca9c7ab544aa4862f94c4508cc7
David <kpdmmvi@gmail.com>
2025-11-06 21:10:32 -0500

    Added definition for feedback period

--- src/TEAM21Proj4A.c
+++ src/TEAM21Proj4A.c
@@ -5,6 +5,7 @@
 #define PWM_FREQ_HZ 50 // Standard servo PWM frequency
 #define TIM3_FREQ_HZ 1000000 // 1MHz timer clock for 1us resolution
 #define TIM3_ARR ((TIM3_FREQ_HZ / PWM_FREQ_HZ) - 1) // Auto-reload value for 50Hz (20000 - 1 = 19999)
+#define FEEDBACK_PERIOD_US 1100
 #define SERVO_NEUTRAL_PULSE_WIDTH 1500 // 1.5ms pulse width for neutral position
 
 #define CW_MAX_PULSE 1480
